b160e5a18e45af6b9b6cfbe978f12ece07ea65d7
ma24anb <ma24anb@herts.ac.uk>
2026-03-31 21:40:22 +0100

    Empty Class files

---
+++ share_item/src/main/java/com/mycompany/share_item/Share_item.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.share_item;
+
+/**
+ *
+ * @author ayadm
+ */
+public class Share_item {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
