6127b9b0779597f1448d5ac1c341809951177297
ma24anb <ma24anb@herts.ac.uk>
2026-04-01 22:22:40 +0100

    Updated collection class added some functions

--- share_item/src/main/java/com/mycompany/share_item/Collection.java
+++ share_item/src/main/java/com/mycompany/share_item/Collection.java
@@ -4,10 +4,38 @@
  */
 package com.mycompany.share_item;
 
+import java.util.*;
+
 /**
  *
  * @author ayadm
  */
 public class Collection {
+    private  ArrayList<Item> borrowing = new ArrayList<>(); 
     
+    
+    public Collection(){
+    }
+    
+    public void addBook(String title, String author, Member donator, 
+            String language, String isbn){
+        
+    }
+    
+    public void addDVD(String title, String author, Member donator, 
+            String language, String isbn ){
+        
+    }
+    
+    public ArrayList<Item> searchItems(String searchItem){
+        return null;
+    }
+    
+    public Item getItem (String title){
+        return null;
+    }
+    
+    public void removeItem(Item item){
+        
+    }
 }
