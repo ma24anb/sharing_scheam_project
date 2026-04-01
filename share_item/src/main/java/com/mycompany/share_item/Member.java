6fefb498022bf0db3dc35f52179b80fa60fc558e
ma24anb <ma24anb@herts.ac.uk>
2026-04-01 19:13:48 +0100

    Updated member class added some functions

--- share_item/src/main/java/com/mycompany/share_item/Member.java
+++ share_item/src/main/java/com/mycompany/share_item/Member.java
@@ -4,10 +4,80 @@
  */
 package com.mycompany.share_item;
 
+import java.util.*;
+
 /**
  *
  * @author ayadm
  */
 public class Member {
+    private String name;
+    private String address;
+    private String email;
+    private final int donatedQty;
+    private  ArrayList<Item> borrowing = new ArrayList<>();
+    private  ArrayList<Item> donatedItems = new ArrayList<>();
+    
+    public Member (String name,String address,String email, int donatedQty){
+        
+        this.name = name;
+        this.address = address;
+        this.donatedQty = 0;
+    }
+    
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setName(String name){
+        this.name = name; 
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public int getDonatedQty (){
+        return donatedQty;
+    }
+    
+    public ArrayList<Item> getDonatedItems(){
+        return donatedItems;
+    }
+    public int borrowingQty(){
+        return borrowing.size();
+    }
+    public ArrayList<Item> getLoanItems(){
+        return borrowing;
+    }
+    
+    public void lend(Item item){
+        // lend max of 5 items 
+    }
+    
+    public void addDonation(Item item){
+        // tbc
+    }
+    
+    public void returnItem(Item item){
+        //tbc
+    }
+    
+    public String toString(){
+        return null;
+    }
+    
     
 }
