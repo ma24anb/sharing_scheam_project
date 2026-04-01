7c928abb5fbb3c703691d094286568f5d891f515
sa25ald <sa25ald@herts.ac.uk>
2026-04-02 00:57:19 +0100

    dvd

--- share_item/src/main/java/com/mycompany/share_item/DVD.java
+++ share_item/src/main/java/com/mycompany/share_item/DVD.java
@@ -8,6 +8,47 @@ package com.mycompany.share_item;
  *
  * @author ayadm
  */
-public class DVD {
-    
+public class DVD extends Item {
+
+    // Attributes (from UML)
+    private String director;
+    private int duration; // in minutes
+
+    // Constructor
+    public DVD(String title, String director, Member donatedBy, String language, int duration) {
+        super(title, language, donatedBy);
+        this.director = director;
+        this.duration = duration;
+    }
+
+    // Getter for director
+    public String getDirector() {
+        return director;
+    }
+
+    // Setter for director
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    // Getter for duration
+    public int getDuration() {
+        return duration;
+    }
+
+    // Setter for duration
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "DVD{" +
+                "title='" + getTitle() + '\'' +
+                ", director='" + director + '\'' +
+                ", language='" + getLanguage() + '\'' +
+                ", duration=" + duration + " mins" +
+                '}';
+    }
 }
