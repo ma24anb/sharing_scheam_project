c1bcbbfe374bf1731dc4efc5667896126a8303ab
sa25ald <sa25ald@herts.ac.uk>
2026-04-02 00:51:12 +0100

    item

--- share_item/src/main/java/com/mycompany/share_item/Item.java
+++ share_item/src/main/java/com/mycompany/share_item/Item.java
@@ -10,4 +10,72 @@ package com.mycompany.share_item;
  */
 public class Item {
     
-} //test  
+
+
+    // Attributes
+    private String title;
+    private String language;
+    private Member donatedBy;
+    private Member onLoanTo;
+
+    // Constructor
+    public Item(String title, String language, Member donatedBy) {
+        this.title = title;
+        this.language = language;
+        this.donatedBy = donatedBy;
+        this.onLoanTo = null; // initially not on loan
+    }
+
+    // Getter for title
+    public String getTitle() {
+        return title;
+    }
+
+    // Setter for title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter for language
+    public String getLanguage() {
+        return language;
+    }
+
+    // Setter for language
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    // Loan item to a member
+    public void loanTo(Member borrower) {
+        if (isAvailable()) {
+            this.onLoanTo = borrower;
+            borrower.borrowItem(this); // update member record
+        } else {
+            System.out.println("Item is already on loan.");
+        }
+    }
+
+    // Check availability
+    public boolean isAvailable() {
+        return onLoanTo == null;
+    }
+
+    // Return the item
+    public void returnLoan() {
+        if (onLoanTo != null) {
+            onLoanTo.returnItem(this); // update member record
+            this.onLoanTo = null;
+        }
+    }
+
+    // Get donor
+    public Member getDonator() {
+        return donatedBy;
+    }
+
+    // Clear donor info
+    public void clearDonator() {
+        this.donatedBy = null;
+    }
+}
