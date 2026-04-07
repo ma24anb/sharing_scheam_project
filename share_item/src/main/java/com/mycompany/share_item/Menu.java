e38b22fc6ee53819da9ddb4bf7b09e68859ca185
nw25aao <nw25aao@herts.ac.uk>
2026-04-07 09:59:40 +0100

    v1

---
+++ share_item/src/main/java/com/mycompany/share_item/Menu.java
@@ -0,0 +1,148 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.share_item;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nohimhasitha
+ */
+public class Menu {
+
+    private String[] menuOptionsSet; // where the options are stored
+    private int[] validNumRange; // create the number range for each options set
+    private boolean zeroBasedIndexing = false;
+    Scanner input = new Scanner(System.in);
+
+    public Menu(String[] menuOptionsSet) {
+        this.menuOptionsSet = menuOptionsSet;
+        this.validNumRange = new int[menuOptionsSet.length];
+
+        for (int range = 0; range < validNumRange.length; range++) { // this for loop automatically defines the range when an object is initialised.
+            validNumRange[range] = range + 1;
+        }
+    }
+
+    public Menu(String[] menuOptionsSet, boolean returnOption) {
+        this.menuOptionsSet = new String[menuOptionsSet.length + 1];
+        for (int i = 0; i < menuOptionsSet.length; i++) {
+            this.menuOptionsSet[i] = menuOptionsSet[i];
+        }
+        this.menuOptionsSet[this.menuOptionsSet.length - 1] = "Return to previous menu";
+        this.validNumRange = new int[this.menuOptionsSet.length];
+        for (int range = 0; range < validNumRange.length; range++) { // this for loop automatically defines the range when an object is initialised.
+            validNumRange[range] = range + 1;
+        }
+    }
+
+    public boolean isEmpty() {
+        return menuOptionsSet.length == 0;
+    }
+
+    // a method to check if a number is in the allowed range
+    // this is used throughout the program to check if users input is valid
+    public boolean isInValidNumRange(int option) {
+        boolean isInRange = false;
+        for (int num : this.validNumRange) {
+
+            if (option == num) {
+                isInRange = true;
+            }
+        }
+        return isInRange;
+    }
+
+    public void displayMenu() {
+
+        String largestStr = "Please select an Option: ";
+        String outputStr = new String();
+
+        for (int option = 0; option < menuOptionsSet.length; option++) {
+
+            outputStr += String.format("%s) %s %n", option + 1, menuOptionsSet[option]);
+            if (menuOptionsSet[option].length() > largestStr.length()) {
+                largestStr = menuOptionsSet[option];
+            }
+
+        }
+
+        System.out.println("");
+        System.out.printf("%s %s %s%n", "=".repeat(largestStr.length() / 2), "MENU", "=".repeat(largestStr.length() / 2));
+        System.out.print(outputStr);
+        System.out.printf("%s%s%s%n", "=".repeat(largestStr.length() / 2), "======", "=".repeat(largestStr.length() / 2));
+
+        if (zeroBasedIndexing) {
+            System.out.println("0: Select to go to Main menu.");
+        }
+        System.out.print("Please Select an option: ");
+    }
+
+    public boolean validateInputRange(int option) {
+        boolean isInRange = false;
+
+        if (option < 0) {
+            return false;
+        }
+
+        for (int number : validNumRange) {
+
+            if (number == option) {
+
+                isInRange = true;
+                break;
+            }
+
+        }
+
+        return isInRange;
+    }
+
+    public void displayErrorMessage() {
+        String errorStr = "The Selected option is Incorrect! Please try again.";
+        String formatStr = "#".repeat(errorStr.length());
+        System.out.printf("%n%s%n%s%n%s%n%n", formatStr, errorStr, formatStr);
+    }
+
+    public void displayErrorMessage(String err) {
+        String errorStr = err;
+        String formatStr = "#".repeat(errorStr.length());
+        System.out.printf("%n%s%n%s%n%s%n%n", formatStr, errorStr, formatStr);
+    }
+
+    public int run() {
+
+        int num = -1; // a variable to hold the selected option
+        boolean isValid = false;
+        do {
+            displayMenu();
+            // this is critical as it ensures only numbers are entered
+            if (input.hasNextInt()) {
+                num = input.nextInt();
+                input.nextLine();
+                isValid = validateInputRange(num);
+
+                if (!isValid) {
+                    displayErrorMessage();
+                }
+            } else {
+                displayErrorMessage("Please enter a number!");
+                input.next();       // Discard invalid token
+            }
+        } while (!isValid);
+
+        return num; // return the validated num
+
+    }
+
+    public boolean isMenuEmpty() {
+        boolean isEmpty = false;
+        if (menuOptionsSet.length == 0) {
+            isEmpty = true;
+        }
+        return isEmpty;
+    }
+
+}
--- share_item/src/main/java/com/mycompany/share_item/Share_item.java
+++ share_item/src/main/java/com/mycompany/share_item/Share_item.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.share_item;
 
 /**
@@ -10,7 +9,80 @@ package com.mycompany.share_item;
  */
 public class Share_item {
 
+    final static String[] mainMenuOptions = {"Manage Items", "Manage members", "Save data", "Exit program"};
+    final static String[] memberMenuOptions = {"Add member", "Update member", "Remove member"};
+    final static String[] itemMenuOptions = {"Search items", "Add item", "Update item", "Remove item", "Lend item to member", "Return item to collection"};
+
+    public static void addMember() {
+    }
+
+    public static void updateMember() {
+    }
+
+    public static void removeMember() {
+    }
+
+    public static void searchItems() {
+    }
+
+    public static void addItem() {
+    }
+
+    public static void updateItem() {
+    }
+
+    public static void removeItem() {
+    }
+
+    public static void lendItem() {
+    }
+
+    public static void returnItem() {
+    }
+
+    public static void manageItems() {
+        Menu itemMenu = new Menu(itemMenuOptions, true);
+        int selectedItemsMenuOption = itemMenu.run();
+        if (selectedItemsMenuOption == itemMenuOptions.length) {
+            return;
+        }
+    }
+
+    public static void manageMembers() {
+        Menu memberMenu = new Menu(memberMenuOptions, true);
+        int selectedMemberMenuOption = memberMenu.run();
+        if (selectedMemberMenuOption == memberMenuOptions.length) {
+            return;
+        }
+    }
+
+
+    public static void saveData() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello !");
+
+        Menu mainMenu = new Menu(mainMenuOptions);
+        int selectedOption = mainMenu.run();
+        while (selectedOption != 4) {
+
+            switch (selectedOption) {
+                case 1:
+                    manageItems();
+                    break;
+                case 2:
+                    manageMembers();
+                    break;
+                case 3:
+                    saveData();
+                case 4:
+                    System.out.println("exit");
+            }
+
+            selectedOption = mainMenu.run();
+
+        }
+
     }
 }
